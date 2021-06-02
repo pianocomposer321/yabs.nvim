d19d0e8c16e5200fa036c516c05b5117dd2f6b13
pianocomposer321 <tmarklebw88@gmail.com>
2021-06-02 08:01:39 -0500

    !!BREAKING CHANGE!! Make setup function more intuitive

--- init.lua
+++ init.lua
@@ -14,9 +14,43 @@ local M = {
     did_setup = false
 }
 
+M.Language = require("yabs/language")
+
 function M:setup(opts)
     self.build_func = U.create_config(opts).build_func
+    local languages = opts.languages or {}
+
+    for name, build_command in pairs(languages) do
+        local default = false
+        local override = false
+        local command
+
+        if type(build_command) == "table" then
+            if build_command.default ~= nil then default = build_command.default end
+            if build_command.override ~= nil then override = build_command.override end
+            command = build_command[1]
+        else
+            command = build_command
+        end
+
+        local language = self.Language:new {
+            name = name,
+            command = command
+        }
+        language:setup(self, {
+            default = default,
+            override = override
+        })
+    end
     self.did_setup = true
+end
+
+function M:add_language(name, command, override, default)
+    local language = M.Language:new {
+        name = name,
+        command = command
+    }
+    language:setup(self, {override = override, default = default})
 end
 
 function M:build()
@@ -57,7 +91,5 @@ function M:load_config_file()
         return false
     end
 end
-
-M.Language = require("yabs/language")
 
 return M
