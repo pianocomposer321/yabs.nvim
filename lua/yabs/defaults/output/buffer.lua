874cea0ff33db048fe30a1bbda541160a17cc4f7
pianocomposer321 <tmarklebw88@gmail.com>
2021-09-05 16:02:36 -0500

    Improve default output handlers

--- lua/yabs/defaults/output/buffer.lua
+++ lua/yabs/defaults/output/buffer.lua
@@ -1,7 +1,50 @@
-local function buffer(cmd)
-    vim.cmd("bot 13new")
-    vim.fn.termopen(cmd)
-    vim.cmd("starti")
+local function make_scratch_buffer(height, position)
+    if not height then
+        height = ""
+    end
+    if not position then
+        position = ""
+    else
+        position = position .. " "
+    end
+    vim.cmd(position .. height .. "new")
+
+    vim.opt_local.buftype = "nofile"
+    vim.opt_local.bufhidden = "wipe"
+    vim.opt_local.buflisted = false
+    vim.opt_local.swapfile = false
+    vim.opt_local.wrap = false
+
+    return vim.fn.bufnr()
 end
+
+local function append_to_buffer(buffer, lines)
+    vim.api.nvim_buf_set_lines(buffer, -2, -2, false, lines)
+end
+
+local bufnr
+
+local function on_exit()
+end
+
+local function on_read(lines)
+    lines[#lines] = nil
+    append_to_buffer(bufnr, lines)
+end
+
+local function buffer(cmd)
+    bufnr = make_scratch_buffer(14, "bot")
+
+    require("yabs/util").async_command(cmd, {
+        on_exit = on_exit,
+        on_read = vim.schedule_wrap(on_read)
+    })
+end
+
+-- local function buffer(cmd)
+--     vim.cmd("bot 13new")
+--     vim.fn.termopen(cmd)
+--     vim.cmd("starti")
+-- end
 
 return buffer
--- lua/yabs/defaults/output/quickfix.lua
+++ lua/yabs/defaults/output/quickfix.lua
@@ -1,52 +1,25 @@
-local stdout
-local stderr
-local handle
-
 local results = {}
 
 local function on_exit()
-    stdout:read_stop()
-    stderr:read_stop()
-
-    stdout:close()
-    stderr:close()
-
-    handle:close()
-
-    vim.fn.setqflist({}, " ", {title = "yabs.nvim output", lines = results})
+    vim.fn.setqflist({}, " ", {lines = results, nr = "$"})
+    results = {}
 end
 
-local function on_read(err, data)
-    assert(not err, err)
+local function on_read(lines)
+    for index, line in ipairs(lines) do
+        if index == #lines and line == "" then goto continue end
 
-    if data then
-        local lines = vim.split(data, "\n")
-        for index, line in ipairs(lines) do
-            if index == #lines and line == "" then goto continue end
+        table.insert(results, line)
 
-            table.insert(results, line)
-
-            ::continue::
-        end
+        ::continue::
     end
 end
 
 local function quickfix(cmd)
-    local loop = vim.loop
-
-    local args = vim.split(cmd, " ")
-    cmd = table.remove(args, 1)
-
-    stdout = loop.new_pipe()
-    stderr = loop.new_pipe()
-
-    handle = loop.spawn(cmd, {
-        stdio = {nil, stdout, stderr},
-        args = args
-    }, vim.schedule_wrap(on_exit))
-
-    loop.read_start(stdout, on_read)
-    loop.read_start(stderr, on_read)
+    require("yabs/util").async_command(cmd, {
+        on_exit = on_exit,
+        on_read = on_read
+    })
 end
 
 return quickfix
