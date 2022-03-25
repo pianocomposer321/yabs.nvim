36dcbb1028c73ac4856da6d6602bc5913a02fd1b
pianocomposer321 <tmarklebw88@gmail.com>
2022-03-25 13:19:36 -0400

    feat: M.run_task now functions!

--- lua/yabs.lua
+++ lua/yabs.lua
@@ -1,13 +1,71 @@
+if not Debugging then Debugging = function() end end
+
 local M = {}
 
-local Language = require("yabs.language")
+local languages
+local tasks
+local outputs
+local placeholders
 
 M.setup = function(config)
-  for name, language_ in pairs(config.languages) do
-    print(name)
-    language_.name = language_.name or name
-    local language = Language(language_)
+  languages = config.languages or {}
+  tasks = config.tasks or {}
+  outputs = config.outputs or {}
+  placeholders = config.placeholders or {}
+
+  for key, language in pairs(languages) do
+    if placeholders then
+      languages[key].placeholders = setmetatable(language.placeholders or {},
+        { __index = placeholders })
+    end
+
+    if tasks then
+      languages[key].tasks = setmetatable(language.tasks or {}, { __index = tasks })
+    end
+
+    if outputs then
+      languages[key].outputs = setmetatable(language.outputs or {}, { __index = outputs })
+    end
   end
+end
+
+local get_output_name = function(language, task)
+  return languages[language].tasks.__outputs[task]
+end
+
+local get_cur_lang = function()
+  return vim.api.nvim_buf_get_option(0, "filetype")
+end
+
+M.run_task = function(task_name, args)
+  local cur_lang_name = get_cur_lang()
+  local cur_lang = languages[cur_lang_name]
+  local command = cur_lang.tasks[task_name]
+  local output_name = get_output_name(cur_lang_name, task_name)
+  local output = cur_lang.outputs[output_name]
+  output(command)
+end
+
+if Debugging() then
+  M.setup {
+    languages = {
+      lua = {}
+    },
+    tasks = {
+      say_hi = "echo 'hi'",
+      __outputs = {
+        say_hi = "echo"
+      }
+    },
+    outputs = {
+      echo = function(cmd, args)
+        vim.cmd("!" .. cmd)
+      end,
+      cmd = vim.cmd
+    }
+  }
+
+  M.run_task("say_hi")
 end
 
 return M
