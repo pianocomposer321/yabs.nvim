58da1c6c5a292a2f29497fcedc7c16eb9aa7af4d
pianocomposer321 <tmarklebw88@gmail.com>
2022-04-13 17:07:29 -0400

    update tests

--- lua/yabs/test.lua
+++ lua/yabs/test.lua
@@ -1,44 +1,10 @@
 local yabs = require("yabs")
-local Output = require("yabs.output")
-local Runner = require("yabs.runner")
-
-local system = function(command, output)
-  local result = vim.fn.system(command)
-  output:recieve(result)
-end
-
-local Echo = Output:new()
-
-function Echo:recieve(data)
-  local opts = self.opts or {}
-  if opts.inspect then
-    P(data)
-  else
-    print(data)
-  end
-end
-
-local System = Runner:new()
-
-function System:run(output)
-  local command = self.command
-  local args = self.args
-  if args then
-    command = vim.tbl_flatten {command, args}
-  end
-  local data = vim.fn.system(command)
-  output:recieve(data)
-end
-
-yabs.register_runner("system", System)
-yabs.register_output("echo", Echo)
 
 require("yabs-plenary").setup()
-local Quickfix = require("yabs-defaults.outputs.quickfix")
-yabs.register_output("quickfix", Quickfix)
+require("yabs-defaults").setup()
 
 yabs.run_commands {
   { {"bash", "-c", "echo start && sleep 2 && echo end"}, "plenary", "quickfix" },
   { {"bash", "-c", "echo hello, world"}, "plenary", "quickfix" },
-  { {"bash", "-c", "echo hello, world"}, "system", {"echo", inspect = true} }
+  { {"bash", "-c", "echo hi"}, "system", { "echo", inspect = true } }
 }
