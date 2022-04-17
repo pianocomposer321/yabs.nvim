f9b88b3eb7e034d56eaaa6201cd330f8c5e1e340
pianocomposer321 <tmarklebw88@gmail.com>
2022-04-17 17:56:17 -0400

    add ex runner

--- lua/yabs/defaults.lua
+++ lua/yabs/defaults.lua
@@ -1,17 +1,17 @@
-local yabs = require("yabs.core")
+local yabs_core = require("yabs.core")
 
 local M = {}
 
 function M.setup()
-  local runners = {"terminal", "system"}
+  local runners = {"terminal", "system", "ex"}
   local outputs = {"quickfix", "echo"}
 
   for _, runner in ipairs(runners) do
-    yabs.register_runner(runner, require("yabs.defaults.runners." .. runner))
+    yabs_core.register_runner(runner, require("yabs.defaults.runners." .. runner))
   end
 
   for _, output in ipairs(outputs) do
-    yabs.register_output(output, require("yabs.defaults.outputs." .. output))
+    yabs_core.register_output(output, require("yabs.defaults.outputs." .. output))
   end
 end
 
