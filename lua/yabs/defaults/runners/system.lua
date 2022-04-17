be100dddfd9c940f00d1b53fdab5d38fca50a4f1
pianocomposer321 <tmarklebw88@gmail.com>
2022-04-17 17:53:47 -0400

    support status in system runner

--- lua/yabs/defaults/runners/system.lua
+++ lua/yabs/defaults/runners/system.lua
@@ -2,7 +2,12 @@ local Runner = require("yabs.core.runner")
 
 local System = Runner:new()
 
+function System:init()
+  self.status = self.__statuses.IDLE
+end
+
 function System:run(output)
+  self:set_status(self.__statuses.RUNNING)
   local command = self.command
   local args = self.args
   if args then
@@ -10,6 +15,7 @@ function System:run(output)
   end
   local data = vim.fn.system(command)
   output:recieve(data)
+  self:set_status(self.__statuses.EXITED)
 end
 
 return System
