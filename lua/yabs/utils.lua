a9491c82cc7d532dedb74729732cb9f7f7e8eb71
pianocomposer321 <tmarklebw88@gmail.com>
2022-04-17 17:55:12 -0400

    move extract_command_and_args to utils module

--- lua/yabs/utils.lua
+++ lua/yabs/utils.lua
@@ -3,12 +3,27 @@ local M = {}
 function M.extract_name_and_opts(opts)
   local opts_type = type(opts)
   if opts_type == "string" then
-    return opts
+    return opts, {}
   elseif opts_type == "table" then
     local name = opts[1]
     opts[1] = nil
     return name, opts
   end
+  return opts
+end
+
+function M.extract_command_and_args(command)
+  local args
+  local command_type = type(command)
+  if command_type == "string" then
+    local split = vim.split(command, " ")
+    command = split[1]
+    args = vim.list_slice(split, 2, #split)
+  elseif command_type == "table" then
+    args = vim.list_slice(command, 2, #command)
+    command = command[1]
+  end
+  return command, args or {}
 end
 
 return M
